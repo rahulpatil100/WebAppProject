fe1e0243266370dc1abaa779e2f018a0fdf1df59
Rahul Patil <rahulcs629@gmail.com>
2018-07-11 23:35:49 +0530

    commited

---
+++ todo.js
@@ -0,0 +1,69 @@
+var ul = document.getElementById('list');
+var li;
+
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', addItem);
+
+var removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', removeItem)
+
+
+
+
+function  addItem(){
+  var input = document.getElementById('input')
+  var item = input.value;
+
+  ul = document.getElementById('list');
+  var textnode = document.createTextNode(item);
+
+
+if (item === '') {
+  return false;
+  //Add a p tag and assign value of "Enter your todo"
+}
+else{
+  //create li
+  li = document.createElement('li');
+
+  //create checkbox
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.setAttribute('id', 'check');
+
+  //create label
+  var label = document.createElement('label');
+   label.setAttribute('for','item') //optional
+
+
+    // add this elements web page
+
+    ul.appendChild(label);
+    li.appendChild(checkbox);
+    label.appendChild(textnode);
+    li.appendChild(label);
+    ul.insertBefore(li, ul.childNodes[0]);
+
+
+   //setTimeout is visula effect for adding item 2 milisec
+    setTimeout(() => {
+      li.className = 'visual';
+    }, 2)
+
+    input.value = '';
+
+}
+
+
+}
+
+
+function removeItem(){
+  li = ul.children
+  for (var index = 0; index <li.length; index++) {
+    while (li[index] && li[index].children[0].checked) {
+      ul.removeChild(li[index])
+    }
+  }
+}
